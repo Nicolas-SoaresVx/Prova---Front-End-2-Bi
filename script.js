f9db503860faf63895fc1b71f5dd1df3b97393fb
Nicolas-SoaresVx <soaresnicolas874@gmail.com>
2026-06-17 12:19:03 -0300

    Tentativa (em pouco tempo) para colocar validação na segunda tela

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function validar(){
         error.innerText = "E-mail inválido ou senha menor de 8 caracteres";
     }
 }
+
+function salvarInscricao(){
+    const senha1 = document.getElementById('senha-cadastro').value;
+    const senha2 = document.getElementById('confirma-senha').value;
+    const email = document.getElementById('email-cadastro').value;
+    const error = document.querySelector('error-resposta');
+
+    if(senha1 == senha2){
+        alert("Inscrição salva com sucesso!")
+        error.innerText = " ";
+    }else{
+        error.innerText = "Senha NÂO pode ser DIFERENTE"
+    }
+
+    if(email.includes("@gmail.com")){
+        alert("Email válido")
+        error.innerText = " ";
+    }else{
+        error.innerText = "E-mail inválido";
+    }
+}
