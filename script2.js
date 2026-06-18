d5d1275ee3c37012c2a1ca410405f614065b21ff
Nicolas-SoaresVx <soaresnicolas874@gmail.com>
2026-06-18 11:23:25 -0300

    Criando a array bidimensional com 12 cursos

---
+++ script2.js
@@ -0,0 +1,17 @@
+function verificar() {
+
+    const cursos = [
+     ["Engenharia Civil", "Exatas"],
+     ["Ciência da Computação", "Exatas"],
+     ["Matemática", "Exatas"],
+     ["Direito", "Humanas"],
+     ["História", "Humanas"],
+     ["Geografia", "Humanas"],
+     ["Medicina", "Natureza"],
+     ["Biologia", "Natureza"],
+     ["Enfermagem", "Natureza"],
+     ["Letras", "Linguagens"],
+     ["Jornalismo", "Linguagens"],
+     ["Publicidade", "Linguagens"]
+    ]
+}
