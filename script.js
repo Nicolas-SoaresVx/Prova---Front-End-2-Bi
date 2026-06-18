84f820482e1fd0c54f7d1237bdf02333ad664757
Nicolas-SoaresVx <soaresnicolas874@gmail.com>
2026-06-18 12:15:35 -0300

    'git add .' para nao deixar nada pra tras

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function validar(){
     if(email.includes("@gmail.com") && senha.length >= 8){
         alert("Email válido")
         error.innerText = " ";
-        window.location.href("painel.html")
+        window.location.href = "painel.html";
     }else{
         error.innerText = "E-mail inválido ou senha menor de 8 caracteres";
     }
